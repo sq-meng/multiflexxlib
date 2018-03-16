13b99212556d643c4d3c4e2b534c0b440c53c782
Siqin Meng <mengsq04@gmail.com>
2018-03-16 14:45:29 +0100

    added res files properly

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ requires = ['pyclipper>=1.1.0',
             'matplotlib>=2.1.0']
 
 setup(name='multiflexxlib',
-      version='0.1',
+      version='0.1.0.dev2',
       description='Tools library for CAMEA neutron detector MultiFLEXX',
       url='http://github.com/yumemi5k/multiflexxlib',
       author='Siqin Meng',
       author_email='mengsq04@gmail.com',
       license='',
       packages=['multiflexxlib'],
-      zip_safe=False)
+      zip_safe=False,
+      include_package_data=True)
