fb86c8f6ca4a245afd1b99ab7ae068cbbd44e4ea
Siqin Meng <mengsq04@gmail.com>
2018-04-09 23:36:45 +0200

    version 0.2.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requires = ['pyclipper>=1.1.0',
             'scipy>=1.0.0']
 
 setup(name='multiflexxlib',
-      version='0.2.5',
+      version='0.2.6',
       description='Tools library for CAMEA neutron detector MultiFLEXX',
       url='http://github.com/yumemi5k/multiflexxlib',
       author='Siqin Meng',
