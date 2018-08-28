3fa6d431fbea8b4e19bdf1d1164ac95d34f281fe
Siqin Meng <mengsq04@gmail.com>
2018-08-28 14:40:49 +0800

    Modified an example

--- examples/drawing_to_existing_ax.py
+++ examples/drawing_to_existing_ax.py
@@ -2,8 +2,22 @@
 import multiflexxlib as mfl
 import matplotlib.pyplot as plt
 df = mfl.read_and_bin()
-f, ax = plt.subplots(1)
+f, axes = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True)
 
+ax = axes[0, 0]
 mfl.draw_voronoi_patch(ax, df.data.loc[2])  # Reference a row in DataFrame
 ax.set_aspect(df.ub_matrix.figure_aspect)  # Set display aspect to be equal in absolute reciprocal length.
+
+ax = axes[0, 1]
+mfl.draw_scatter(ax, df.data.loc[2])
+ax.set_aspect(df.ub_matrix.figure_aspect)
+
+ax = axes[1, 0]
+mfl.draw_voronoi_patch(ax, df.data.loc[2], mesh=True)
+ax.set_aspect(df.ub_matrix.figure_aspect)
+
+ax = axes[1, 1]
+mfl.draw_interpolated_patch(ax, df.data.loc[2])
+ax.set_aspect(df.ub_matrix.figure_aspect)
+f.tight_layout()
 plt.show()
